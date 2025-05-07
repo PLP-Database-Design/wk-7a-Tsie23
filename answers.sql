7819c244c035d689f8c94708cc66b104dfc21c56
Tsie23 <ka.sennelo@gmail.com>
2025-05-07 05:12:10 +0200

    Week 7 Assignment Revised Submission

--- answers.sql
+++ answers.sql
@@ -1,49 +1,52 @@
 -- Question 1 Achieving 1NF (First Normal Form)
 -- Create a table adhering to 1NF
-CREATE TABLE productDetail_1NF (
-    OrderID INT PRIMARY KEY AUTO_INCREMENT,
-    CustomerName VARCHAR(100) NOT NULL,
-    Product VARCHAR(100) NOT NULL
+-- Create a table for ProductDetail
+CREATE TABLE ProductDetail (
+    OrderID INT,
+    CustomerName VARCHAR(100),
+    Products VARCHAR(100)
 );
 
--- Insert data into the 1NF-compliant table
-INSERT INTO productDetail_1NF (CustomerName, Product)
+-- Insert data into the Product Detail table
+INSERT INTO ProductDetail (OrderID, CustomerName, Product)
 VALUES
-('John Doe', 'Laptop'),
-('John Doe', 'Mouse'),
-('Jane Smith', 'Tablet'),
-('Jane Smith', 'Keyboard'),
-('Jane Smith', 'Mouse'),
-('Emily Clark', 'Phone');
+(101, 'John Doe', 'Laptop'),
+(101, 'John Doe', 'Mouse'),
+(102, 'Jane Smith', 'Tablet'),
+(102, 'Jane Smith', 'Keyboard'),
+(102, 'Jane Smith', 'Mouse'),
+(103, 'Emily Clark', 'Phone');
 
 -- Question 2 Achieving 2NF (Second Normal Form)
 -- Create a table adhering to 2NF
-CREATE TABLE customers_new(
-    customers_id INT PRIMARY KEY AUTO_INCREMENT,
-    customer_name VARCHAR(100) NOT NULL
+-- Create a table for Orders
+CREATE TABLE Orders(
+    OrderID INT PRIMARY KEY,
+    CustomerName VARCHAR(100),
 );
 
--- Insert data into the customers table
-INSERT INTO customers_new (customer_name)
+-- Insert data into the Orders table
+INSERT INTO Orders (OrderID, CustomerName)
 VALUES
-('John Doe'),
-('Jane Smith'),
-('Emily Clark');
+(101, 'John Doe'),
+(102, 'Jane Smith'),
+(103, 'Emily Clark');
 
-CREATE TABLE orderDetails_2NF(
-    order_id INT PRIMARY KEY AUTO_INCREMENT,
-    customers_id INT NOT NULL,
-    product VARCHAR(100) NOT NULL,
-    quantity INT NOT NULL,
-    FOREIGN KEY (customers_id) REFERENCES customers_new(customers_id)
+-- Create a table for Product
+CREATE TABLE Product (
+    OrderID INT,
+    Product VARCHAR(100),
+    Quantity INT,
+    PRIMARY KEY (OrderID, Product),
+    FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
 );
 
--- Insert data into the orderDetails_2NF table
-INSERT INTO orderDetails_2NF (customers_id, product, quantity)
+-- Insert data into the Product table
+INSERT INTO Product (OrderID, Product, Quantity)
 VALUES
-(01, 'Laptop', 2),
-(01, 'Mouse', 1),
-(02, 'Tablet', 3),
-(02, 'Keyboard', 1),
-(02, 'Mouse', 2),
-(03, 'Phone', 1);
+(101, 'Laptop', 2),
+(101, 'Mouse', 1),
+(102, 'Tablet', 3),
+(102, 'Keyboard', 1),
+(102, 'Mouse', 2),
+(103, 'Phone', 1);
